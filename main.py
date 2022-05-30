1d082aa9b5966081d8be582f344eb926aba324da
luisvitorpg <99150383+luisvitorpg@users.noreply.github.com>
2022-05-30 10:23:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot("!")
+client = commands.Bot("!")
 
-@bot.event
+@client.event
 async def on_ready():
   print(f"Estou pronto! rodando no bot {bot.user}")
 
-@bot.command(name="ping")
+@client.command(name="ping")
 async def ping(ctx):
   await ctx.send("Pong!")
 
-@bot.command(name="twitch")
+@client.command(name="twitch")
 async def twitch(ctx):
   await ctx.send("minha twitch: https://twitch.tv/thetannereel")
 
-@bot.command(name="rickroll")
+@client.command(name="rickroll")
 async def rickroll(ctx):
   await ctx.send("Rick Roll! https://bit.ly/39NAz2s")
-@bot.command()
+@client.command()
 async def RJ(ctx):
   await ctx.send("a capital de RJ e Rio de Janeiro")
-@bot.command()
+@client.command()
 async def SP(ctx):
   await ctx.send("a capital de SP e Sao Paulo")
-@bot.command()
+@client.command()
 async def GO(ctx):
   await ctx.send("a capital de GO e Goiania")
 
-bot.run('OTgwMTY5NTEzNDYwOTY5NTQz.GY6boi.Xqjc-jQICqF5eUE3wvQP7dMewTa9MmToGHR0Lg')
+client.run('OTgwMTY5NTEzNDYwOTY5NTQz.GY6boi.Xqjc-jQICqF5eUE3wvQP7dMewTa9MmToGHR0Lg')
